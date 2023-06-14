09e5365174a8fadb57081ea272ebaa2d6ede79a1
minaunal <minaunal12312sd@gmail.com>
2023-06-14 14:45:37 +0300

    gereksiz seyler silindi

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: {
         "/": (context) => Iskele(),
-        "/anasayfa": (context) => AnaSayfa()
+       // "/anasayfa": (context) => AnaSayfa()
       },
     );
   }
